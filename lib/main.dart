40c88e16a2fef8b15c63bf063dbaf54161ebfdc5
Graham Murray <GLMurray@student.fullsail.edu>
2024-05-18 23:31:31 -0500

    1. Added new login screen 2. Updated input text screen 3. Fixed screen to match sign up and figma.

--- lib/main.dart
+++ lib/main.dart
@@ -4,23 +4,20 @@ import 'package:flutter/rendering.dart';
 import 'Global_Elements/textbox.dart';
 import 'Global_Elements/ui_tiles.dart';
 import 'Screens/sign_up_screen.dart';
+import 'package:terra_tutor/Screens/login_screen.dart';
 
-void main() 
-{
+void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget 
-{ 
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) 
-  {
+  Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData
-      (
+      theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -29,8 +26,7 @@ class MyApp extends StatelessWidget
   }
 }
 
-class MyHomePage extends StatefulWidget
-{
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
   final String title;
@@ -39,53 +35,41 @@ class MyHomePage extends StatefulWidget
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> 
-{
+class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
-  void _incrementCounter() 
-  {
-    setState(() 
-    {
+  void _incrementCounter() {
+    setState(() {
       _counter++;
     });
   }
 
   @override
-  Widget build(BuildContext context) 
-  {
-    return Scaffold
-    (
-      appBar: AppBar
-      (
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center
-      (
-        child: Column
-        (
+      body: Center(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>
-          [
-            const Text
-            (
+          children: <Widget>[
+            const Text(
               'You have pushed the button this many times:',
             ),
-            Text
-            (
+            Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton
-      (
+      floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ),
     );
   }
-} 
+}
