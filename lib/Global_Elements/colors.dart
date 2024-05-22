2c2cdd21aceb8dd7fc6ce7e0e1b0e962afafb671
Madison Walker <madwalker231@gmail.com>
2024-05-22 13:41:25 -0500

    Color file for default color theme.

---
+++ lib/Global_Elements/colors.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class AppColors
+{
+  // Background color
+  static const Color primaryColor = Color(0x00e2d9c7);
+
+  // Widget tile color
+  static const Color uiTile = Color(0x00FBF26C);
+
+  // Top and bottom Nav bar color
+  static const Color navBar = Color(0x00C3D161);
+
+  // Menu icon highlight box color
+  static const Color menuIconHighLight = Color(0x00FFFFFF); // Transparency will be ajusted upon icon tsting
+
+  // Menu icon colors
+  static const Color menuIconClicked = Color(0x00000000);
+  static const Color menuIconUnclicked = Color(0x007F7F7F);
+
+  // Font color
+  static const Color fontColor = Color(0x00000000);
+}
