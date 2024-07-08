1d81313995cd7037f43b206a41ba2703b6206046
Madison Walker <madwalker231@gmail.com>
2024-07-08 10:18:06 -0500

    Conflict fix.

--- lib/Global_Elements/ui_tiles.dart
+++ lib/Global_Elements/ui_tiles.dart
@@ -24,6 +24,7 @@ class UiTile extends StatelessWidget {
   final BoxFit imageFit;
   final TextAlignOption textAlignment;
   final bool hasPhotos;
+  final Widget? child;
 
   UiTile({
     super.key,
@@ -33,8 +34,8 @@ class UiTile extends StatelessWidget {
     required this.name,
     this.description,
     required this.textAlignment,
-    this.width = 300,
-    this.height = 200,
+    this.width = 160,
+    this.height = 180,
     this.margin = const EdgeInsets.only(left: 30),
     this.borderRadius = const BorderRadius.all(Radius.circular(12)),
     this.backgroundColor = AppColors.uiTile,
@@ -53,6 +54,7 @@ class UiTile extends StatelessWidget {
     ),
     this.imageFit = BoxFit.cover,
     this.hasPhotos = false,
+    this.child,
   }) {
     assert(textAlignment == TextAlignOption.center ||
         textAlignment == TextAlignOption.topLeft);
@@ -61,7 +63,7 @@ class UiTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(10.0),
+      padding: const EdgeInsets.all(20.0),
       child: Container(
         width: width,
         height: height,
@@ -122,14 +124,14 @@ class UiTile extends StatelessWidget {
                               description!,
                               style: descriptionTextStyle,
                             ),
+                            if (child != null)
+                            child!,
                         ],
                       ),
                     ),
+                  ),
                 ],
               ),
-            ),
-          ],
-        ),
       ),
     );
   }
